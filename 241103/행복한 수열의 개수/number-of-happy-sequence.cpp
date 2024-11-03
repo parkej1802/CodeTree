5bd68b05ddd6253a4270179a00a9c96d9640d6e3
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-03 07:43:00 -0400

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241103/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 241103/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 
 using namespace std;
 
 int checkRowCol(int n, int m, vector<vector<int>> matrix) {
 
     int result = 0;
-    unordered_map<int, int> mapX;
-    unordered_map<int, int> mapY;
+    int countX = 1, countY = 1;
 
-    for (int row = 0; row < n; row++) {
+    for (int row = 1; row < n; row++) {
         
-        for (int col = 0; col < n; col++) {
-            mapX[matrix[row][col]]++;
-            mapY[matrix[col][row]]++;
-        }
-        for (int i = 0; i < mapX.size(); i++) {
-            if (mapX[matrix[row][i]] == m) {
-                result++;
-                break;
+        for (int col = 1; col < n; col++) {
+            if (matrix[row][col] == matrix[row - 1][col - 1]) {
+                countX++;
+                if (countX >= m) {
+                    result++;
+                }
+            }
+            else {
+                countX = 1;
+            }
+
+            if (matrix[col][row] == matrix[col - 1][row - 1]) {
+                countY++;
+                if (countY >= m) {
+                    result++;
+                }
+            }
+            else {
+                countY = 1;
             }
         }
-        for (int j = 0; j < mapY.size(); j++) {
-            if (mapY[matrix[j][row]] == m) {
-                result++;
-                break;
-            }
-        }
 
         
-        mapX.clear();
-        mapY.clear();
+        
     }
     return result;
 }
@@ -53,5 +55,6 @@ int main() {
     int result = checkRowCol(n, m, matrix);
 
     cout << result;
+
     return 0;
 }
