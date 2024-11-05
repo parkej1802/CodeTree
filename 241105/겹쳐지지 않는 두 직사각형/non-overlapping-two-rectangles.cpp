c7f1c2c0f17934e8e7740fe303f874bca5cbbc54
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-05 04:30:08 -0400

    Title: 겹쳐지지 않는 두 직사각형, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241105/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
+++ 241105/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
@@ -19,25 +19,22 @@ int twoRectangleSum(int n, int m, vector<vector<int>> matrix) {
 
     int globalMaxSum = INT_MIN;
     for (int i1 = 0; i1 < n; i1++) {
-        for (int j1 = 0; j1 < n; j1++) {
+        for (int j1 = 0; j1 < m; j1++) {
             for (int down1 = i1; down1 < n; down1++) {
-                for (int right1 = j1; right1 < n; right1++) {
+                for (int right1 = j1; right1 < m; right1++) {
                     
                     int firstSum = calcRecAreaSum(i1, j1, down1, right1, matrix);
                     
                     for (int i2 = 0; i2 < n; i2++) {
-                        for (int j2 = 0; j2 < n; j2++) {
-                            if (i2 > down1 || j2 > down1 || i2 > right1 || j2 > right1) {
-                                for (int down2 = i2; down2 < n; down2++) {
-                                    for (int right2 = j2; right2 < n; right2++) {
-                                        if (!((i1 <= down2 && down1 >= i2) && (j1 <= right2 && right1 >= j2))) {
-                                            int secondSum = calcRecAreaSum(i2, j2, down2, right2, matrix);
-                                            globalMaxSum = max(globalMaxSum, firstSum + secondSum);
-                                        }
+                        for (int j2 = 0; j2 < m; j2++) {
+                            for (int down2 = i2; down2 < n; down2++) {
+                                for (int right2 = j2; right2 < m; right2++) {
+                                    if (!((i1 <= down2 && down1 >= i2) && (j1 <= right2 && right1 >= j2))) {
+                                        int secondSum = calcRecAreaSum(i2, j2, down2, right2, matrix);
+                                        globalMaxSum = max(globalMaxSum, firstSum + secondSum);
                                     }
                                 }
                             }
-                            
                         }
                     }
                  }
