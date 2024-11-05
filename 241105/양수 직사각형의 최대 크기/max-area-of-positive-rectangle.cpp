1564b8a1b51f01deaadd39e9ba37d620089e5670
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-05 04:51:52 -0400

    Title: 양수 직사각형의 최대 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int calRecArea(int startX, int startY, int endX, int endY, vector<vector<int>> matrix) {
+
+    for (int i = startX; i <= endX; i++) {
+        for (int j = startY; j <= endY; j++) {
+            if (matrix[i][j] < 0) {
+                return 0;
+            }
+        }
+    }
+    return (endX - startX + 1) * (endY - startY + 1);
+}
+
+int getMaxRecArea(int n, int m, vector<vector<int>> matrix) {
+    
+    int maxArea = 0;
+    int valid = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int down = i; down < n; down++) {
+                for (int right = j; right < m; right++) {
+                    valid = calRecArea(i, j, down, right, matrix);
+                    if (valid != 0) {
+                        maxArea = max(valid, maxArea);
+                    }
+                } 
+            }
+        }
+    }
+    return maxArea;
+}
+
+int main() {
+    
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<vector<int>> matrix(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> matrix[i][j];
+
+    
+    int result = getMaxRecArea(n, m, matrix);
+
+    cout << result;
+
+    return 0;
+}
