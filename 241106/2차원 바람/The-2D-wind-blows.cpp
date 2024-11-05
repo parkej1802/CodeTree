94233d5722d537ab43095a8ee58400978437619b
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-05 13:18:24 -0400

    Title: 2차원 바람, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/2차원 바람/The-2D-wind-blows.cpp
+++ 241106/2차원 바람/The-2D-wind-blows.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-vector<vector<int>> twoDimensionalWind(int n, int m, vector<vector<int>>& matrix, int q, int r1, int c1, int r2, int c2) {
+vector<vector<int>> twoDimensionalWind(int n, int m, vector<vector<int>>& matrix, int q, vector<int> r1, vector<int> c1, vector<int> r2, vector<int> c2) {
     
     vector<vector<int>> initialMatrix = matrix;
 
     for (int numWind = 0; numWind < q; numWind++) {
-        int topLeftY = r1 - 1;
-        int topLeftX = c1 - 1;
-        int bottomRightY = r2 - 1;
-        int bottomRightX = c2 - 1;
+        int topLeftY = r1[numWind] - 1;
+        int topLeftX = c1[numWind] - 1;
+        int bottomRightY = r2[numWind] - 1;
+        int bottomRightX = c2[numWind] - 1;
 
         int temp = matrix[topLeftY][topLeftX];
 
@@ -54,7 +54,7 @@ vector<vector<int>> twoDimensionalWind(int n, int m, vector<vector<int>>& matrix
         }
         matrix = initialMatrix;
     }
-    return matrix;
+    return initialMatrix;
 }
 
 int main() {
@@ -70,9 +70,20 @@ int main() {
         }
     }
 
-    cin >> r1 >> c1 >> r2 >> c2;
+    vector<int> x1;
+    vector<int> x2;
+    vector<int> y1;
+    vector<int> y2;
+    for (int i = 0; i < q; i++) {
+        cin >> r1 >> c1 >> r2 >> c2;
+        x1.push_back(r1);
+        x2.push_back(r2);
+        y1.push_back(c1);
+        y2.push_back(c2);
+    }
+    
 
-    vector<vector<int>> result = twoDimensionalWind(n, m, matrix, q, r1, c1, r2, c2);
+    vector<vector<int>> result = twoDimensionalWind(n, m, matrix, q, x1, y1, x2, y2);
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
