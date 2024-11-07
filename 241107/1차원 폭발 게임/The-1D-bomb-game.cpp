deaceaaebdc3cb34de4011dd68a679ad3df28668
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-07 06:58:49 -0400

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241107/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 241107/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -4,13 +4,14 @@
 #include <algorithm>
 using namespace std;
 
-void oneDimensionalBomb(int n, int m, vector<int>& bombs) {
+vector<int> oneDimensionalBomb(int n, int m, vector<int>& bombs) {
     
     bool loop = true;
-    
+    vector<int> result;
+
     while (loop) {
         int bombSize = bombs.size();
-        for (int i = 0; i < bombs.size(); i++) {
+        for (int i = 0; i < n; i++) {
             int count = 1;
             for (int j = 1; j < bombs.size(); j++) {
                 if (i + j < bombs.size()) {
@@ -26,17 +27,15 @@ void oneDimensionalBomb(int n, int m, vector<int>& bombs) {
 
             if (count >= m) {
                 bombs.erase(bombs.begin() + i, bombs.begin() + i + count);
+                i++;
             }
             
         }
-        if (bombSize == bombs.size()) {
-            loop = false;
-        }
-
-        if (bombs.empty()) {
+        if (bombSize == bombs.size() || bombs.empty()) {
             loop = false;
         }
     }
+    return bombs;
 }
 
 int main() {
@@ -50,11 +49,11 @@ int main() {
         cin >> bombs[i];
     
 
-    oneDimensionalBomb(n, m, bombs);
+    vector<int> result = oneDimensionalBomb(n, m, bombs);
 
     cout << bombs.size() << endl;
-    for (int i = 0; i < bombs.size(); i++) {
-        cout << bombs[i] << endl;
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << endl;
     }
 
     return 0;
