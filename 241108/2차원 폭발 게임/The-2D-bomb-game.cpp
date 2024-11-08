fa3d73eb9fd36b3b0699a338efd34b9b1de92253
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-08 06:55:14 -0400

    Title: 2차원 폭발 게임, Time: 35ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241108/2차원 폭발 게임/The-2D-bomb-game.cpp
+++ 241108/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -32,7 +32,7 @@ void explodeBombs(vector<vector<int>>& matrix, int n, int m) {
                 count++;
             } 
             else {
-                if (count >= m) {
+                if (count >= m && !stk.empty()) {
                     for (int i = 0; i < count; i++) {
                         stk.pop();
                     }
@@ -42,7 +42,7 @@ void explodeBombs(vector<vector<int>>& matrix, int n, int m) {
             stk.push(matrix[row][col]);
         }
         
-        if (count >= m) {
+        if (count >= m && !stk.empty()) {
             for (int i = 0; i < count; i++) {
                 stk.pop();
             }
@@ -64,15 +64,13 @@ void explodeBombs(vector<vector<int>>& matrix, int n, int m) {
 
 void rotateMatrix(vector<vector<int>>& matrix, int n) {
 
-    for (int i = 0; i < n / 2; i++) {
-        for (int j = i; j < n - i - 1; j++) {
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[n - j - 1][i];
-            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-            matrix[j][n - i - 1] = temp;
+    vector<vector<int>> rotated(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            rotated[j][n - i - 1] = matrix[i][j];
         }
     }
+    matrix = rotated;
 }
 
 int twoDimensionalExplosionGame(int n, int m, int k, vector<vector<int>>& matrix) {
