cd9dbf6e0d4cf7e63ebea1f74bdfdcc9b52a2bc7
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-18 03:08:19 -0400

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241118/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[1001];
+
+int stair(int n) {
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 1;
+    dp[5] = 2;
+    dp[6] = 2;
+    dp[7] = 3;
+
+    for (int i = 8; i <= n; i++) {
+        dp[i] = dp[i - 2] + dp[i - 3];
+    }
+
+    return dp[n] % 10007;
+}
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    int result = stair(n);
+    cout << result;
+    return 0;
+}
