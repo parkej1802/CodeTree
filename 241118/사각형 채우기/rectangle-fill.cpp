17c167d02ac4a90f508fc06fbdf8565d609d070f
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-18 03:11:11 -0400

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241118/사각형 채우기/rectangle-fill.cpp
+++ 241118/사각형 채우기/rectangle-fill.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int dp[1001];
 
 int rect(int n) {
-    dp[0] = 0;
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 3;
 
     for (int i = 4; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] = dp[i] % 10007;
     }
+
     return dp[n] % 10007;
 }
 
