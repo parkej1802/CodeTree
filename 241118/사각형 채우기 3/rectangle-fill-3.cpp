d1a61bf7a7755e1c2534601471f62897074b5e85
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-18 05:20:09 -0400

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241118/사각형 채우기 3/rectangle-fill-3.cpp
+++ 241118/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int dp[1001];
+long dp[1001];
 
-int rec(int n) {
+long rec(int n) {
     dp[0] = 1;
     dp[1] = 2;
     dp[2] = 7;
@@ -11,12 +11,11 @@ int rec(int n) {
     
     for (int i = 3; i <= n; i++) {
         dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3;
-
+        dp[i] = dp[i] % 1000000007;
         for (int j = i - 3; j >= 0; j--) {
             dp[i] = dp[i] + 2 * dp[j];
+            dp[i] = dp[i] % 1000000007;
         }
-      
-        dp[i] = dp[i] % 1000000007;
     }
 
     return dp[n];
@@ -29,7 +28,7 @@ int main() {
 
     cin >> n;
 
-    int result = rec(n);
+    long result = rec(n);
 
     cout << result;
 
