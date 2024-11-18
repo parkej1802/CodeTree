4acc2e9ea7d23894f073fb2265003c47fcbf2ab3
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-18 02:25:38 -0400

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241118/피보나치 수/fibonacci-number.cpp
+++ 241118/피보나치 수/fibonacci-number.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int dp[46];
+vector<int> memo;
 
-int fibo(int n) {
-    dp[1] = 1;
-    dp[2] = 1;
-
-    for (int i = 3; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+int fibbo(int n) {
+    if (memo[n] != -1) {
+        return memo[n];
     }
-
-    return dp[n];
+    if (n <= 2) {
+        memo[n] = 1;
+    }
+    else {
+        memo[n] = fibbo(n - 1) + fibbo(n - 2);
+    }
+    return memo[n];
 }
 
 int main() {
     int n;
     cin >> n;
+    memo.resize(n + 1, -1);
 
-    int result = fibo(n);
+    int result = fibbo(n);
 
     cout << result;
     
