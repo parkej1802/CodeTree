3c0fa2486eca1042e7686537344d91762b5ac65e
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-22 08:28:21 -0400

    Title: 알바로 부자 되기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241122/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+int n, s, e, p;
+
+vector<pair<int, int>> list;
+vector<int> pay;
+vector<int> dp;
+
+int maxProfit() {
+
+    dp[0] = pay[0];
+    int result = dp[0];
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+           
+            if (list[i].first > list[j].second) {
+                dp[i] = max(dp[i], dp[j] + pay[i]);
+                result = max(result, dp[i]);
+            }
+            result = max(result, pay[i]);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    cin >> n;
+
+    dp.resize(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        cin >> s >> e >> p;
+
+        list.push_back({s, e});
+        pay.push_back(p);
+    }
+
+
+    int result = maxProfit();
+
+    cout << result;
+
+    return 0;
+}
