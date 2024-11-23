91ded03b4fb4b96e2edb62c414b9274ca0aaa27b
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-23 14:19:25 -0400

    Title: 부분 수열의 합이 m, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241124/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int n, m, k;
+
+vector<int> list;
+vector<int> dp;
+
+int minSumSeq() {
+    dp[0] = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = m; j > 0; j--) {
+            if (j >= list[i]) {
+                if (dp[j - list[i]] == INT_MAX) continue;
+
+                dp[j] = min(dp[j], dp[j - list[i]] + 1);
+
+            
+            }
+            
+        }
+    }
+    return dp[m];
+}
+
+int main() {
+    
+    cin >> n >> m;
+
+    dp.resize(m + 1, INT_MAX);
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+
+        list.push_back(k);
+    }
+
+    int result = minSumSeq();
+
+    if (result = INT_MAX) {
+        cout << -1;
+    }
+    else {
+        cout << result;
+    }
+    
+
+    return 0;
+}
