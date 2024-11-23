2cfb7c2e548c6f8e1ecff4e3a4428065d57103c7
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-23 14:36:08 -0400

    Title: 1, 2, 5 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241124/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+
+vector<int> dp;
+vector<int> list;
+
+int oneTwoFiveSum() {
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < list.size(); j++) {
+            if (i >= list[j]) {
+                dp[i] += dp[i - list[j]];
+            }
+        }
+    }
+
+    return dp[n] % 10007;
+}
+
+int main() {
+
+    cin >> n;
+
+    dp.resize(n + 1, 0);
+    
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(5);
+
+    int result = oneTwoFiveSum();
+
+    cout << result;
+
+    return 0;
+}
