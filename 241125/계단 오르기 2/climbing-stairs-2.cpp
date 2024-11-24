c1a40f35aadd916b34c2031c7fbff874c1851c48
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-24 15:11:34 -0400

    Title: 계단 오르기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241125/계단 오르기 2/climbing-stairs-2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+vector<vector<int>> dp;
+vector<int> list;
+
+int n;
+
+void stair() {
+
+    dp[0][0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        dp[i][0] = dp[i - 1][0];
+        for (int j = 1; j <= 3; j++) {
+
+           if (dp[i - 1][j - 1] != INT_MIN) {
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + list[i]);
+            }
+
+            if (i > 2 && dp[i - 2][j] != INT_MIN) {
+                dp[i][j] = max(dp[i][j], dp[i - 2][j] + list[i]);
+            }
+
+        }
+    }
+
+    cout << dp[n][3];
+}
+
+int main() {
+    
+    cin >> n;
+
+    list.resize(n + 1);
+    dp.resize(n + 1, vector<int>(4, INT_MIN));
+
+    for (int i = 1; i <= n; i++) {
+        cin >> list[i];
+    }
+
+    stair();
+    
+    return 0;
+}
