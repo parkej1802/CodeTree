2568abd46ee6a60d74ffdc52fd8c0023f014d057
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-11-26 10:56:34 -0400

    Title: 구슬의 이동, Time: 14ms, Memory: 1MB, Status: Passed - CodeTree

--- 241126/구슬의 이동/marble-movement.cpp
+++ 241126/구슬의 이동/marble-movement.cpp
@@ -1,138 +1,108 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
+#include <unordered_map>
 #include <algorithm>
-#include <set>
 
 using namespace std;
 
-int n, m, t, k, r, c, s;
-char d;
+int dir[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
 
-vector<tuple<int, int, int, int, int>> marbles;
+int n, m, t, k;
 
-int dy[4] = {-1, 0, 0, 1};
-int dx[4] = {0, 1, -1, 0};
+struct Marble {
+    int row, col;
+    int d;
+    int id;
+    int speed;
+};
 
-int direction[128];
+unordered_map<char, int> map = {
+    {'U', 0}, {'R', 1}, {'D', 2}, {'L', 3}
+};
+
+vector<Marble> marbles;
 
 bool inRange(int row, int col) {
-    return row > 0 && row <= n && col > 0 && col <= n;
+    return row >= 0 && row < n && col < n && col >= 0;
 }
 
-bool compare(pair<int, int>& a, pair<int, int>& b) {
-    if (a.second != b.second) {
-        return a.second > b.second;
+bool compare(Marble m1, Marble m2) {
+    if (m1.speed != m2.speed) {
+        return m1.speed > m2.speed;
     }
-    
-    return a.first > b.first;
+    return m1.id > m2.id;
 }
 
-tuple<int, int, int, int, int> moveMarbles(tuple<int, int, int, int, int> marble) {
+void collision() {
 
-    int x, y, dir, speed, id;
-    
-    tie(y, x, dir, speed, id) = marble;
+    vector<vector<vector<Marble>>> matrix(n, vector<vector<Marble>>(n));
 
-    for (int i = 1; i <= speed; i++) {
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
+    sort(marbles.begin(), marbles.end(), compare);
 
-        if (inRange(nx, ny)) {
-            x = nx;
-            y = ny;
-        }
-        else {
-            dir = (dir + 3) % 4;
-            x = x + dx[dir];
-            y = y + dy[dir];
-        }
-        
+    for (auto& mar : marbles) {
+        matrix[mar.row][mar.col].push_back(mar);
     }
 
-    return make_tuple(y, x, dir, speed, id);
-}
+    vector<Marble> newMarble;
 
-void checkCollision() {
-
-    set<int> toRemove;
-    vector<pair<int, int>> ids;
-
-    for (int i = 0; i < m; i++) {
-        int x, y, dir, speed, id;
-        tie(y, x, dir, speed, id) = marbles[i];
-        
-        for (int j = 0; j < m; j++) {
-            
-            if (i == j) continue;
-
-            int ny, nx, ndir, nspeed, nid;
-            tie(ny, nx, ndir, nspeed, nid) = marbles[j];
-
-            if (y == ny && x == nx) {
-               
-                ids.push_back({nid, nspeed});
-
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!matrix[i][j].empty()) {
+                int size = min((int)matrix[i][j].size(), k);
+                for (int a = 0; a < size; a++) {
+                    newMarble.push_back(matrix[i][j][a]);
+                }
             }
         }
+    }
 
-        if (ids.empty()) continue;
-        
-        ids.push_back({id, speed});
-        sort(ids.begin(), ids.end(), compare);
-        
-        if (ids.size() > k) {
-            int index = 1;
-            for (int j = k; j < ids.size(); j++) {
-                int mid = ids[ids.size() - index++].first;
-                toRemove.insert(mid);
-            }
-        }
-        ids.clear();
-        
-    }
-    
-    for (int num : toRemove) {
-        auto it = find_if(marbles.begin(), marbles.end(), [&](const tuple<int, int, int, int, int>& m) {
-            return get<4>(m) == num;
-        });
-        
-        marbles.erase(it);
-        
-    }
+    marbles = newMarble;
 }
 
+void moveMarble() {
 
+    for (auto& mar : marbles) {
+        for (int i = 0; i < mar.speed; i++) {
+            int ny = mar.row + dir[mar.d][0];
+            int nx = mar.col + dir[mar.d][1];
+
+            if (inRange(ny, nx)) {
+                mar.row = ny;
+                mar.col = nx;
+            }
+            else {
+                mar.d = (mar.d + 2) % 4;
+                ny = mar.row + dir[mar.d][0];
+                nx = mar.col + dir[mar.d][1];
+                mar.row = ny;
+                mar.col = nx;
+            }
+        }
+    }
+}
 
 void simulate() {
-
-    for (int i = 0; i < t; i++) {
-        for (int j = 0; j < marbles.size(); j++) {
-            marbles[j] = moveMarbles(marbles[j]);
-            
-        }
-        checkCollision();
-    }
-
-    cout << marbles.size(); 
+    moveMarble();
+    collision();
 }
 
 int main() {
-    direction['U'] = 0;
-    direction['R'] = 1;
-    direction['L'] = 2;
-    direction['D'] = 3;
-    
     cin >> n >> m >> t >> k;
 
     for (int i = 0; i < m; i++) {
-        cin >> r >> c >> d >> s;
-        marbles.push_back(make_tuple(r, c, direction[d], s, i));
+        int tr, tc, ts;
+        char td;
+
+        cin >> tr >> tc >> td >> ts;
+
+        marbles.push_back({ tr - 1, tc - 1, map[td], i, ts });
     }
 
-    simulate();
+    while (t--) {
+        simulate();
+    }
 
-
+    cout << marbles.size();
 
     return 0;
 }
