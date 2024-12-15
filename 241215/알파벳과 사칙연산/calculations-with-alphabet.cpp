85c6a19c645fb5a7642164b64b4b799fbb7aae94
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-12-15 08:05:48 -0400

    Title: 알파벳과 사칙연산, Time: 1003ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241215/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 241215/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -1,58 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <string>
 #include <climits>
 
 using namespace std;
 
-int max = 0;
 vector<char> alpha;
-vector<char> sign;  
-vector<int> values;
-int maxValue = INT_MIN;
+vector<char> sign;
+unordered_map<char, int> map;
+int maxValue = INT_MIN; 
 
-int getValue() {
-    int result = values[0];
+int calculate(int a, int b, char op) {
+    if (op == '+') return a + b;
+    if (op == '-') return a - b;
+    if (op == '*') return a * b;
+    return 0;
+}
 
+int getResult() {
+    int result = map[alpha[0]];
     for (int i = 0; i < sign.size(); ++i) {
-        if (sign[i] == '+') result += values[i + 1];
-        else if (sign[i] == '-') result -= values[i + 1];
-        else if (sign[i] == '*') result *= values[i + 1];
+        result = calculate(result, map[alpha[i + 1]], sign[i]);
     }
-
     return result;
 }
 
 void backTrack(int index) {
     if (index == alpha.size()) {
-        int result = getValue();
-        maxValue = std::max(maxValue, result);
+        int result = getResult();
+        maxValue = max(maxValue, result);
         return;
     }
 
     for (int num = 1; num <= 4; ++num) {
-        values[index] = num;
+        map[alpha[index]] = num;
         backTrack(index + 1);
     }
 }
 
 int main() {
     string input;
-
     cin >> input;
 
     for (char ch : input) {
         if (ch == '+' || ch == '-' || ch == '*') {
             sign.push_back(ch);
-        }
-        else {
+        } else {
             alpha.push_back(ch);
         }
     }
 
-    values.resize(alpha.size(), 0);
     backTrack(0);
 
-    cout << maxValue;
-    
+    cout << maxValue << endl;
     return 0;
 }
