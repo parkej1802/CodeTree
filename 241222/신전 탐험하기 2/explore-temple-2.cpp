2a703f2126def0fa52835b12eaa7d4c1ce973098
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-12-22 02:31:21 +0900

    Title: 신전 탐험하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241222/신전 탐험하기 2/explore-temple-2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<vector<vector<int>>> dp;
+vector<vector<int>> list;
+
+void explor() {
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                if (j != k) {
+                    for (int m = 0; m < 3; m++) {
+                        dp[i][k][m] = max(dp[i][k][m], list[i][k] + dp[i - 1][k][m]);
+                    }
+                }
+            }
+        }
+    }
+    
+    int maxResult = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i != j) {
+                maxResult = max(maxResult, dp[n - 1][j][i]);
+            }
+        }
+    }
+
+    cout << maxResult;
+}
+
+int main() {
+    cin >> n;
+
+    dp.resize(n, vector<vector<int>>(3, vector<int>(3, 0)));
+    list.resize(n, vector<int>(3));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> list[i][j];
+        }
+    } 
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            dp[0][i][j] = list[i][j];
+        }
+    }
+
+    explor();
+
+    return 0;
+}
