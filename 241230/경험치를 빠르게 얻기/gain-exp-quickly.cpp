8be5e4ff93ce0b4aaece0f3e856395e6cea53a92
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2024-12-30 23:56:11 +0900

    Title: 경험치를 빠르게 얻기, Time: 8ms, Memory: 1MB, Status: Passed - CodeTree

--- 241230/경험치를 빠르게 얻기/gain-exp-quickly.cpp
+++ 241230/경험치를 빠르게 얻기/gain-exp-quickly.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -8,39 +9,40 @@ vector<pair<int, int>> quests;
 vector<int> dp;
 
 int n, m;
-int maxExp = 1000000; 
 
 int main() {
-
     cin >> n >> m;
 
     quests.resize(n);
-    dp.resize(maxExp + 1, 1e9);
+    int totalExp = 0;
 
     for (int i = 0; i < n; i++) {
         cin >> quests[i].first >> quests[i].second;
+        totalExp += quests[i].first;
     }
 
+    dp.resize(totalExp + 1, INT_MAX);
     dp[0] = 0;
 
     for (int i = 0; i < n; i++) {
         int exp = quests[i].first;
         int time = quests[i].second;
-        for (int j = exp; j <= maxExp; j++) {
-            dp[j] = min(dp[j], dp[j - exp] + time);
+        for (int j = totalExp; j >= exp; j--) {
+            if (dp[j - exp] != INT_MAX) {
+                dp[j] = min(dp[j], dp[j - exp] + time);
+            }
         }
     }
 
-    int result = 1e9;
-    for (int i = m; i <= maxExp; ++i) {
+    int result = INT_MAX;
+    for (int i = m; i <= totalExp; i++) {
         result = min(result, dp[i]);
     }
 
-    if (result == 1e9) {
+    if (result == INT_MAX) {
         cout << -1;
-    } 
-    else {
-        cout << result << endl;
+    } else {
+        cout << result;
     }
 
     return 0;
