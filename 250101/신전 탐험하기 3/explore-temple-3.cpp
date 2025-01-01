186db14e761b34166c9ca14ddee4b05d847bb86b
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2025-01-01 15:55:28 +0900

    Title: 신전 탐험하기 3, Time: 48ms, Memory: 1MB, Status: Passed - CodeTree

--- 250101/신전 탐험하기 3/explore-temple-3.cpp
+++ 250101/신전 탐험하기 3/explore-temple-3.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int n, l, m, r;
+int n, m;
 
 vector<vector<int>> list;
 vector<vector<int>> dp;
@@ -12,8 +12,8 @@ int maxResult = 0;
 void getMaxValue() {
 
     for (int i = 2; i <= n; i++) {
-        for (int j = 1; j <= 3; j++) {
-            for (int k = 1; k <= 3; k++) {
+        for (int j = 1; j <= m; j++) {
+            for (int k = 1; k <= m; k++) {
                 if (j != k) {
                     dp[i][j] = max(dp[i][j], list[i][j] + dp[i - 1][k]);
                     maxResult = max(maxResult, dp[i][j]);
@@ -26,18 +26,18 @@ void getMaxValue() {
 }
 
 int main() {
-    cin >> n;
+    cin >> n >> m;
 
-    list.resize(n + 1, vector<int>(4, 0));
-    dp.resize(n + 1, vector<int>(4, 0));
+    list.resize(n + 1, vector<int>(m + 1, 0));
+    dp.resize(n + 1, vector<int>(m + 1, 0));
 
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= 3; j++) {
+        for (int j = 1; j <= m; j++) {
             cin >> list[i][j];
         }
     }
 
-    for (int i = 1; i <= 3; i++) {
+    for (int i = 1; i <= m; i++) {
         dp[1][i] = list[1][i];
     }
 
