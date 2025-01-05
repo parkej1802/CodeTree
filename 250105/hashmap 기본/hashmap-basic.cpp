df3a9181c75aff2035a21b8d435ff199bd059c4e
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2025-01-05 23:45:48 +0900

    Title: hashmap 기본, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250105/hashmap 기본/hashmap-basic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int n;
+string str;
+vector<string> functions;
+unordered_map<int, int> map;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        if (isdigit(str)) {
+            
+        }
+    }
+
+    return 0;
+}
