6b567fdc8ac3a91646da292a6fc2597bfed15182
parkej1802 <60753073+parkej1802@users.noreply.github.com>
2025-01-06 11:22:07 +0900

    Title: 두 수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250106/두 수의 합/sum-of-two-num.cpp
+++ 250106/두 수의 합/sum-of-two-num.cpp
@@ -13,19 +13,21 @@ int main() {
     cin >> n >> k;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        map[arr[i]] = arr[i];
+        map[arr[i]]++;
     }
 
     // Write your code here!
 
     for (int i = 0; i < n; i++) {
+        map[arr[i]]--;
         int a = k - arr[i];
         if (map[a]) {
             result++;
         }
     }
 
-    cout << result / 2;
+    cout << result;
 
     return 0;
 }
+
